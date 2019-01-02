d7f45d38072d523b5db3cddfe8021678f28cddf1
srfoster <stephen@thoughtstem.com>
2019-01-02 15:41:19 -0800

    define/contract/doc and rsgv dependency

--- ts-kata-util/info.rkt
+++ ts-kata-util/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "ts-kata-util")
-(define deps '("base" "lang-file"))
+(define deps '("base" "lang-file" "rsvg"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/ts-kata-util.scrbl" ())))
 (define pkg-desc "Description Here")
