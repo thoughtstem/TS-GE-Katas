6e16cb2347969e3e276316fe909f0c7218ec45a3
srfoster <stephen@thoughtstem.com>
2019-01-14 11:17:35 -0800

    started docs and fixing ts-fundamentals sub repo problem

--- ts-kata-util/info.rkt
+++ ts-kata-util/info.rkt
@@ -6,7 +6,7 @@
                "https://github.com/thoughtstem/TS-HomeSite.git" ;For rebuilding the index page
                ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/ts-kata-util.scrbl" ())))
+(define scribblings '(("scribblings/manual.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(thoughtstem))
