065a3d10f85583c1b324bef097e54956226f53de
srfoster <stephen@thoughtstem.com>
2019-01-15 10:07:47 -0800

    temp

--- ts-kata-util/info.rkt
+++ ts-kata-util/info.rkt
@@ -3,7 +3,6 @@
 (define deps '("base" 
                "lang-file" 
                "rsvg" 
-               "https://github.com/thoughtstem/TS-HomeSite.git" ;For rebuilding the index page
                ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/manual.scrbl" ())))
--- ts-kata-util/main.rkt
+++ ts-kata-util/main.rkt
@@ -24,108 +24,9 @@
          define/contract/doc
          )
 
-(define (student-should-know-difference-between . things)
-  (define (show-thing t)
-    (item (bold (first t)) ": "
-          (second t)))
-  
-  (list
-   (para "Students should be able to recite the following definitions from memory:")
-   (apply itemlist (map show-thing things))))
-
-
-
-(define (student-should-translate #:english sentence
-                                  #:code    kata-code-name
-                                  #:lang    lang)
-
-  (list
-   (para "Students should be able to translate any sentence of this type:")
-   (para (italic sentence))
-   (para "To corresponding code of this type:")
-   (show-kata-code lang kata-code-name)))
-
-(define (to-earn-this-code-kata #:english    sentence
-                                #:code       kata-code-name
-                                #:lang       lang
-                                #:badge-type badge-type)
-
-  (list
-   (para "To earn this Kata " badge-type  " students should be able to translate any sentence of this type:")
-   (para (italic sentence))
-   (para "To corresponding code of this type:")
-   (show-kata-code lang kata-code-name)))
-
-
-(define (to-earn-this-rubric-kata #:badge-type    badge-type
-                                  #:game-element  game-element
-                                  #:elements-list elements-list
-                                  #:points-list   points-list)
-
-  (list
-   (para "To earn this Kata " badge-type  " students should be able to recite
-the amount of points they get from having a custom " game-element " in their game.")
-   (para (bold game-element " points:"))
-   (map show-rubric elements-list points-list)))
-
-
-(define (show-rubric element points)
-  (para "* " element " - "
-        (bold (number->string points)
-              (if (= points 1)
-                  " point."
-                  " points."))))
-
-;;;TODO
-(define (to-earn-this-meta-kata   #:badge-type         badge-type
-                                  #:learning-objective learning-objective
-                                  #:objectives         [l '()]
-                                  )
-
-  (list
-   (para "To earn this Kata " badge-type  " students should be able to recite " learning-objective)
-   (map list-points l)))
-
-(define (list-points l)
-  (para "* " l))
-
-
-(define difficulties
-  (hash 'air    (text "AIR" 24 'cyan)
-        'stone  (text "STONE" 24 'gray)
-        'bronze (text "BRONZE" 24 'orange)
-        'silver (text "SILVER" 24 'darkgray)
-        'gold   (text "GOLD" 24 'gold)
-        'platnum   (text "PLATNUM" 24 'blue)))
-
-(define (kata #:document-level (level subsubsection)
-              #:difficulty (difficulty 'bronze)
-              #:title (title "Avatar")
-
-              #:time-limit (time-limit 10)
-              . body)
-
-  (define (render-difficulty d)
-    (string-titlecase (~a d))
-    
-    ;Not working
-    #;(if (hash-has-key? difficulties d)
-        (hash-ref difficulties d)
-        (string-titlecase (~a d))))
-
-  (list
-   
-   (level 
-          (~a (render-difficulty difficulty)
-              " "
-              title
-              " Kata "
-              "(" time-limit " minutes)"))
-   body))
-
-
 
 (require scribble/srcdoc)
+
 
 (define-for-syntax (extract-defaults arguments)
   (define (keyword? d)
@@ -203,6 +104,8 @@ the amount of points they get from having a custom " game-element " in their gam
        (filter (and/c (curryr string-suffix? ".rkt")
                       (not/c (curryr string-suffix? "bak.rkt")))
                (map ~a (directory-list example-folder)))))
+
+
 
 (define example-file-exists?
   (flat-contract-with-explanation
