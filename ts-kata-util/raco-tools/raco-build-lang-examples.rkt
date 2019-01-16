402e5c3615d96e622f748ff506220f85ca072049
srfoster <stephen@thoughtstem.com>
2019-01-16 14:40:46 -0800

    idk

--- ts-kata-util/raco-tools/raco-build-lang-examples.rkt
+++ ts-kata-util/raco-tools/raco-build-lang-examples.rkt
@@ -8,13 +8,13 @@
 (displayln (~a "Building " ts-language-name))
 
 (delete-directory/files	(build-path (current-directory) "examples" "compiled") #:must-exist? #f)
-(delete-directory/files	(build-path (current-directory) "examples" "compiled-kata-data") #:must-exist? #f)
+(delete-directory/files	(build-path (current-directory) "examples" "compiled-example-data") #:must-exist? #f)
 
 (displayln "Compiling Examples, Running Examples as Tests, and building API docs...")
 
 (system (~a "raco test examples " #;ts-language-name 
             " && "
-            "raco setup " ts-language-name 
+            "raco setup -c " ts-language-name 
             ))
 
 (displayln "Done")
