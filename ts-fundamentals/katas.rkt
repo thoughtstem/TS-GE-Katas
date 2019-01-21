f1e96e9902d7fe6d62262e0b3c0198612205e755
Sara Lucchini <saralucchini@Stephens-MacBook-Air-5.local>
2019-01-21 11:03:37 -0800

    to get to master

--- ts-fundamentals/katas.rkt
+++ ts-fundamentals/katas.rkt
@@ -19,6 +19,23 @@
 (define proto-fundamentals
   (lang->kata-collection 'fundamentals))
 
+(define-kata-collection fundamentals-shapes
+  (translate #:id "Another Shape"
+             #:in "Make a star that is gold and size 60 pixels."
+             #:out '(star 60 'solid 'gold))
+  (translate #:id "Favorite Color"
+             #:in "Make a triangle of your favorite color and size 100."
+             #:out '(triangle 100 'solid 'skyblue))
+  (translate #:id "Not Solid?"
+             #:in "Write five copies of the line below, filling in the blank with a word
+                    that DOESN'T work!\n(circle 50 '_______ 'red)"
+             #:out (list '(circle 30 'blue 'red)
+                         '(circle 30 'dotted 'red)
+                         '(circle 30 'line 'red)))
+  (translate #:id "Outline Shape"
+             #:in "Make a square of size 150 with a purple outline."
+             #:out '(triangle 100 'outline 'purple)))
+
 (define-kata-collection fundamentals
   (fill-in-stimuli proto-fundamentals
 
@@ -34,6 +51,7 @@
 (define-kata-collection katas
   unplugged
   fundamentals
+  fundamentals-shapes
   image->code-katas
   simple-pictures)
 
