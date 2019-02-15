d94f4b2334b0c0292cd691b334d0c0dd090d873c
aBlender <JasonLongLe@gmail.com>
2019-02-15 14:46:19 -0800

    Update and recompile battle arena docs

--- ts-battle-arena-asp-2019/katas.rkt
+++ ts-battle-arena-asp-2019/katas.rkt
@@ -13,3 +13,18 @@
    #:out (list "Building a BATTLE ARENA GAME."  
                "In ONE HOUR."
                "Including: an AVATAR, ENEMIES, WEAPONS, and POWERUPS.")))
+
+(define-kata-collection weapon-katas
+  (first (kata-collection-katas single-shot-katas))
+  (first (kata-collection-katas spread-shot-katas))
+  (first (kata-collection-katas homing-repeater-katas))
+  (first (kata-collection-katas sword-katas))
+  (first (kata-collection-katas spear-katas))
+  (first (kata-collection-katas paint-thrower-katas)))
+
+(define-kata-collection powerup-katas
+  (first (kata-collection-katas health-katas))
+  (first (kata-collection-katas shield-katas))
+  (take  (kata-collection-katas boost-katas) 2)
+  (first (kata-collection-katas size-katas)))
+  
