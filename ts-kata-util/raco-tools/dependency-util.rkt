6d0ed721a129070069cbf0a1b562758931b2267b
Stephen <stephen@thoughtstem.com>
2019-03-08 16:13:19 -0800

    somehow lost a function def for try-then, recovering

--- ts-kata-util/raco-tools/dependency-util.rkt
+++ ts-kata-util/raco-tools/dependency-util.rkt
@@ -57,6 +57,15 @@
        (member (build-path "info.rkt")
                (directory-list dir))))
 
+(define (try-then f1 f2)
+  (lambda(x)
+    (current-directory (build-path original x))
+    (with-handlers ([exn:fail? (thunk*
+                                (displayln (~a "Couldn't update " x ", trying to install..."))
+                                (f2 (~a "../" x)))])
+      (displayln (~a "Trying to update " x))
+      (f1 (~a "../" x)))))
+
 (define update-or-install 
   (try-then (curry pkg-update-command  #:link #t #:update-deps #t #:deps 'search-auto)
             (curry pkg-install-command #:link #t #:update-deps #t #:deps 'search-auto)))
