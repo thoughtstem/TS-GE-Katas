32eca3bd5352de78ca1dd00115954a2911fad0a5
Sara Lucchini <saralucchini@Stephens-MacBook-Air-5.local>
2019-04-15 14:54:14 -0700

    k2 sea lang katas

--- ts-k2/katas/read-code-stimuli.rkt
+++ ts-k2/katas/read-code-stimuli.rkt
@@ -211,7 +211,7 @@
    (read-template "game where an apple is trying to survive by eating wolves and dogs, it can collect horses and llamas, and grapes, kiwis, and apples are the enemies.")
    
    'farm-026
-   (read-template "game where a cow is tryng to survive by eating cows, it can collect cows, and a cow is the enemy.")
+   (read-template "game where a cow is trying to survive by eating cows, it can collect cows, and a cow is the enemy.")
 
    'farm-027
    (read-template "game where an animal of your choice is trying to survive by eating 2 food items of your choice, it can collect 3 coins of your choice, and 4 animals of your choice are the enemies.")
@@ -224,5 +224,131 @@
 
    'farm-030
    (read-template "collection of 3 games. You can choose your animal in all three games and it can eat 2 food items of your choice. In the first game you can collect gold, silver in the second, and copper in the third. You will avoid 1 enemy of your choice in the first game, 2 in the second, and 3 in the third. The objective of the game is to score 10 points. Once you reach those points in the first game, you can close it and it will open the following game which has the same objective of 10 points.")
-   ))
+
+
+
+   'sea-001
+   (read-template "game where you are a yellow fish finding and eating pineapples.")
+
+   'sea-002
+   (read-template "game where you are a crab finding and eating kiwi fruit.")
+
+   'sea-003
+   (read-template "game where you are a red fish finding and eating broccoli, tomatoes and apples.")
+
+   'sea-004
+   (read-template "game where you are a starfish finding and eating pineapple and two other foods of your choice.")
+
+   'sea-005
+   (read-template "game where you are a sprite of your choice hunting for three foods of your choice.")
+
+   'sea-006
+   (read-template "game where you are a sprite of your choice hunting for pineapples, broccoli, kiwis, tomatoes and apples.")
+
+   'sea-007
+   (read-template "game where you are an apple hunting for broccoli.")
+
+   'sea-008
+   (read-template "game where you are a pineapple hunting for two foods of your choice.")
+
+   'sea-009
+   (read-template "game where you are a kiwi hunting for three foods of your choice.")
+
+   'sea-010
+   (read-template "game where you are a green fish hunting for other green fish!")
+
+   'sea-011
+   (read-template "game where you are shark hunting for kiwi fruit and collecting copper coins.")
+
+   'sea-012
+   (read-template "game where you are a sprite of your choice hunting for one sprite of your choice and collecting a sprite of your choice.")
+
+   'sea-013
+   (read-template "game where you are an octopus hunting for pineapples and another sprite of your choice, and collecting silver coins and another sprite of your choice.")
+
+   'sea-014
+   (read-template "game where you are a jellyfish hunting for broccoli and apples and collecting gold coins.")
+
+   'sea-015
+   (read-template "game where you are a sprite of your choice hunting for copper coins and collecting apples!")
+
+   'sea-016
+   (read-template "game where you are a sprite of your choice hunting for two sprites of your choice and collecting two sprites of your choice.")
+
+   'sea-017
+   (read-template "game where you are a sprite of your choice hunting for tomatoes and collecting two sprites of your choice.")
+
+   'sea-018
+   (read-template "game where you are a sprite of your choice hunting for two other sprites of your choice and collecting copper, silver and gold coins.")
+
+   'sea-019
+   (read-template "game where you are a broccoli hunting for a sprite of your choice and collecting a sprite of your choice.")
+
+   'sea-020
+   (read-template "game where you are a sprite of your choice hunting for four sprites of your choice and collecting four sprites of your choice.")
+
+   'sea-021
+   (read-template "game where you are a ghost fish hunting for strawberries, collecting silver coins, and avoiding a shark enemy.")
+
+   'sea-022
+   (read-template "game where you are a jellyfish hunting for potatoes and cherries, collecting silver and gold coins, and avoiding a red fish enemy.")
+
+   'sea-023
+   (read-template "game where you are a sprite of your choice hunting for apples and another sprite of your choice, collecting a silver coin and another sprite of your choice, and avoiding an enemy of your choice.")
+
+   'sea-024
+   (read-template "game where you are a shark hunting for apples and strawberries, collecting potatoes and cherries, and avoiding a crab and a jellyfish enemy.")
+
+   'sea-025
+   (read-template "game where you are a strawberry hunting for orange fish and octopus, collecting ghost fish and crabs, and avoiding a potato, and apple and a cherries enemy!")
+
+   'sea-026
+   (read-template "game where you are a shark, hunting for sharks, collecting sharks and avoiding shark enemies!")
+
+   'sea-027
+   (read-template "game where you are a sprite of your choice, hunting for two other sprites of your choice, collecting three sprites of your choice, and avoiding four enemies of your choice.")
+
+   'sea-028
+   (read-template "collection of 2 games. In the first game, you are an octopus hunting for apples, collecting copper coins and avoiding a crab enemy.
+                   In the second game, you are still an octopus, but hunting for cherries, collecting gold coins and avoiding a shark enemy!")
+
+   'sea-029
+   (read-template "collection of 2 games. In the first game, you are a sprite of your choice hunting for strawberries and potatoes, collecting copper and silver coins and avoiding an orange fish enemy.
+                   When you 'level up' in the next game, every thing is the same except you collect silver and gold coins.")
+
+   'sea-030
+   (read-template "collection of 3 games. In all games, you can be the sprite of your choice hunting two sprites of your choice and avoiding two enemies of your choice.
+                   In the first, you collect copper coins, the  silver, then gold in the last game.")
+
+   'sea-031
+   (read-template "game where you are a starfish healing an octopus friend.")
+
+   'sea-032
+   (read-template "game where you are a jellyfish healing  green fish, orange fish and red fish friends.")
+
+   'sea-033
+   (read-template "game where you are a shark healing yellow fish and ghost fish friends.")
+
+   'sea-034
+   (read-template "game where you are a crab healing three other crab friends!")
+
+   'sea-035
+   (read-template "game where you are a sprite of your choice healing red fish, starfish and broccoli friends.")
+
+   'sea-036
+   (read-template "game where you are mushroom healing apple, kiwi, pineapple and broccoli friends!")
+
+   'sea-037
+   (read-template "game where you are a sprite of your choice healing yellow fish and shark friends and two other sprites of your choice.")
+
+   'sea-038
+   (read-template "collection of 2 games. In both games you are a ghost fish. In the first game you are healing starfish and orange fish friends.
+                   In the second game, you are healing a crab friend and three other sprites of your choice.")
+
+   'sea-039
+   (read-template "collection of 2 games. In bothe games you are a sprite of your choice. In the first game you are healing an octopus friend and two other sprites of your choice.
+                   In the second game, you are healing a green fish and two other sprites of your choice.")
+
+   'sea-040
+   (read-template "game where you are a sprite of your choice healing four friends of your choice.")))
 
