9aefa5516cc52d9bcc049e2ab70a8d4cb86c685a
Stephen <stephen@thoughtstem.com>
2019-04-19 16:12:57 -0700

    identifier cards prototype working

--- ts-printing/tiles/main.rkt
+++ ts-printing/tiles/main.rkt
@@ -1,17 +1,85 @@
 #lang racket
 
+(provide begin-identifier-job)
+
 (require ratchet
-         "../main.rkt"
-         (submod k2/lang/hero/powers ratchet))
+         pict
+         (only-in pict/code codeblock-pict)
+         "./double-size.rkt"
+         "../main.rkt")
+
+(define HEIGHT 600)
+(define WIDTH HEIGHT)
 
 
-(define tiles
-  (tangeable-tiles hero-lang))
+;Use this design: https://www.makeplayingcards.com/design/custom-small-square-cards.html
 
-;Kind of works for https://www.boardgamesmaker.com 
-;  But need to make them bigger, and the meta+border didn't fit
-;  Maybe a different product at bgm, or just drop the meta+border
-(for ([t tiles]
-      [i (in-naturals)])
-  (save-pict t (~a "test-" i ".png") 'png))
+
+(define bg 
+  (colorize 
+    (filled-rectangle WIDTH HEIGHT)
+    "white"))
+
+(define (front-side i)
+  (cc-superimpose bg 
+                  i))
+
+(define (back-side i)
+  (cc-superimpose bg
+                  i)) 
+
+(define (identifier->back-side im)
+  (define p  (identifier-mapping-original im)) 
+
+  (front-side 
+    #;
+    (scale p 3)
+    (double-size 
+      (double-size 
+        (pict->bitmap p)))))
+
+
+(define (identifier->front-side im)
+  (define id (identifier-mapping-main im)) 
+ 
+  (back-side
+    (scale 
+      (codeblock-pict (~a id))
+      3)))
+
+(define (identifier->sides i)
+  (list
+    (identifier->front-side i) 
+    (identifier->back-side i)))
+
+
+(define (lang->folder v-lang folder-path)
+  (define ims (visual-language-mappings v-lang))
+
+  (list->folder folder-path
+                (flatten
+                  (map identifier->sides ims))))
+
+
+
+(define (lang->Desktop v-lang folder-name)
+  (lang->folder v-lang
+                (build-path (find-system-path 'home-dir)
+                             "Desktop"
+                             folder-name)))
+
+(define-syntax-rule (begin-identifier-job folder 
+                      (lang [k v] ...) 
+                      ...)
+  (begin
+    (define counter 0)
+
+    (parameterize ([k v] ...
+                   [STARTING-CARD-NUMBER counter]
+                   [META-TRANSFORM (curryr colorize "gray")])
+      (lang->Desktop lang folder)
+      (set! counter (+ counter 
+                       (length 
+                         (visual-language-mappings lang))))) 
+    ...))
 
