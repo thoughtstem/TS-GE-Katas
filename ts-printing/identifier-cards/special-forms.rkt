301cc0f43bf8c8a473938fd1439d5d3aff69208c
Stephen <stephen@thoughtstem.com>
2019-04-25 12:18:52 -0700

    fixing bugs in ts-printing doc to fix travis build

---
+++ ts-printing/identifier-cards/special-forms.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(provide special-image-for)
+
+(require pict)
+(require pict/code)
+
+(define (define->image)
+    (code
+      (define (my-thing ...)
+        ...)))
+
+;Define other special form functions here.
+
+
+(define-namespace-anchor a)
+(define ns (namespace-anchor->namespace a))
+
+(define (special-image-for id)
+  (with-handlers ([exn:fail? (thunk*
+                               (raise (~a "You need to define a function to handle how that special form is displayed on a card: " id)))])
+                 (eval
+                   `(,(string->symbol (~a id "->image")))
+                   ns)))
+
+
