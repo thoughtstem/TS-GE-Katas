c824dddeaf9e000c08fc5f0c6191f763969a566f
Sonny Najar <sonnynajar@gmail.com>
2019-04-26 10:37:01 -0700

    gameshow questions

--- ts-camp-materials/scribblings/gameshow-minecraft.rkt
+++ ts-camp-materials/scribblings/gameshow-minecraft.rkt
@@ -12,7 +12,7 @@ Use these questions during Game Show time. You can ask any/all the questions in 
                     @itemlist[@item{@italic{#:skin}}]}
           @item{@bold{What is the keyword to change what your character looks like?}
                     @itemlist[@item{@italic{#:sprite}}]}
-          @item{@bold{Name three available character sprites.}
+          @item{@bold{Name 3 available character sprites.}
                     @itemlist[@item{@italic{Anything from the Assets Library.}}]}
           @item{@bold{What is the default speed value?}
                     @itemlist[@item{@italic{10.}}]}]
@@ -20,39 +20,39 @@ Use these questions during Game Show time. You can ask any/all the questions in 
 
 @section{Tuesday}
 
-@itemlist[@item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}]
+@itemlist[@item{@bold{How many food items will show up in the game when you use (custom-food)?}
+                    @itemlist[@item{@italic{1.}}]}
+          @item{@bold{By default the food item will heal you by:}
+                    @itemlist[@item{@italic{10.}}]}
+          @item{@bold{What line of code can you add to your food so that they don't reappear in the game once you eat them?}
+                    @itemlist[@item{@italic{#:respawn? #f.}}]}
+          @item{@bold{Name 3 keywords available to customize the food.}
+                    @itemlist[@item{@italic{#:sprite, #:name, #:amount-in-world, #:heals-by, #:respawn?.}}]}
+          @item{@bold{Name 3 available food sprites.}
+                    @itemlist[@item{@italic{Anything from the Assets Library.}}]}]
 
 @section{Wednesday}
 
-@itemlist[@item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}]
+@itemlist[@item{@bold{What is the default value of the ore?}
+                    @itemlist[@item{@italic{10.}}]}
+          @item{@bold{How many ores will spawn in the game by default?}
+                    @itemlist[@item{@italic{10.}}]}
+          @item{@bold{What key do you use to collect the ores?}
+                    @itemlist[@item{@italic{Press the spacebar when you're close to the ore.}}]}
+          @item{@bold{Name 5 ore types from Minecraft.}
+                    @itemlist[@item{@italic{Coal, Iron, Gold, Redstone, Lapis Lazuli, Diamond, Emerald, Nether Quartz.}}]}
+          @item{@bold{Tell me a keyboard shortcut; a combination of keys.}
+                    @itemlist[@item{@italic{CTRL + C, CTRL + V, CTRL + X, CTRL + A, CTRL + Z, CTRL + Y.}}]}]
 
 @section{Thursday}
 
-@itemlist[@item{@bold{What is the keyboard to add enemies?}
+@itemlist[@item{@bold{What is the keyword to add enemies?}
                     @itemlist[@item{@italic{#:mob-list}}]}
           @item{@bold{What does AI stand for?}
                     @itemlist[@item{@italic{Artificial Intelligence.}}]}
           @item{@bold{What is the keyword to change the number of enemies in the game?}
                     @itemlist[@item{@italic{#:amount-in-world}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}
-          @item{@bold{Q?}
-                    @itemlist[@item{@italic{A.}}]}]
+          @item{@bold{What line of code can you add to your mob to make them only apper in night-time?}
+                    @itemlist[@item{@italic{#:night-only? #t.}}]}
+          @item{@bold{Name the 3 available sprites for mobs in the game.}
+                    @itemlist[@item{@italic{creeper-sprite, skeleton-sprite, ghast-sprite.}}]}]
