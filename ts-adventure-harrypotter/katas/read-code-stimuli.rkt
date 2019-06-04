54e07808f11cc4c83e86ab26bb49f84e0efe74ba
Sara Lucchini <saralucchini@Stephens-MacBook-Air-5.local>
2019-06-04 12:34:15 -0700

    mario and hp final stimuli edits

--- ts-adventure-harrypotter/katas/read-code-stimuli.rkt
+++ ts-adventure-harrypotter/katas/read-code-stimuli.rkt
@@ -24,7 +24,7 @@
    (read-prefix "with a fast wizard and an intro cutscene. Customize the cutscene to have a sprite and some text.")
 
    'avatar-4
-   (read-prefix "with a wizard and an intro cutscene. Customize the cutscene with 2 pages: the 1st with text and the 2nd with a sprite and text.")
+   (read-prefix "with a wizard and an intro cutscene. Customize the cutscene with 2 pages, include sprites and text.")
 
    ;Potion stimuli
    'food-1
@@ -37,17 +37,17 @@
    (read-prefix "with a wizard who starts at half health. Add a powerful yellow potion that spawns a cutscene on pickup. The cutscene includes the potion sprite and text.")
 
    'food-4
-   (read-prefix "with several pumpkins and a cauldron that produces a pumpkin potion.")
+   (read-prefix "with several pumpkins and a cauldron that produces a powerful pumpkin potion.")
 
    'food-5
    (read-prefix "with an NPC and a cauldron that produces a carrot potion. Give the NPC dialog and a quest to brew that potion.")
 
    ;Loot quest stimuli
    'loot-quest-1
-   (read-prefix "with an NPC who has a loot quest to retrieve their stolen chest.  Collect the chest by defeating the deatheater who stole it.")
+   (read-prefix "with an NPC who has a loot quest to retrieve their stolen item. Collect the item by defeating the deatheater who stole it.")
 
    'loot-quest-2
-   (read-prefix "with an NPC who has a quest to retrieve their stolen cat from a deatheater. Include a high-damage spell.")
+   (read-prefix "with an NPC who has a quest to retrieve their stolen talking cat from a deatheater. Include a high-damage spell.")
 
    'loot-quest-3
    (read-prefix "with an NPC who has a rewarded quest to retrieve their stolen food. Include a wand.")
@@ -56,36 +56,36 @@
    (read-prefix "with an NPC who has a quest to retrieve their stolen pumpkin. Add quest-complete dialog and a spell.")
 
    'loot-quest-5
-   (read-prefix "with an NPC who has a quest to retrieve their stolen book. Add a pickup cutscene to the flying book.")
+   (read-prefix "with an NPC who has a quest to retrieve their stolen book. Add a pickup cutscene to the stolen book.")
 
    ;NPC stimuli
    'npc-1
-   (read-prefix "with a wizard and a witch NPC that follows you. Give her a name and a specific tile location.")
+   (read-prefix "a witch NPC who follows you. Give her a name and a specific tile location.")
 
    'npc-2
-   (read-prefix "with a wizard and 2 NPCs. Give both NPCs names and multiple lines of dialog.")
+   (read-prefix "2 NPCs. Give both NPCs names and multiple lines of dialog.")
 
    'npc-3
-   (read-prefix "with a wizard and a named NPC. Give the wizard 3 questions and the NPC 3 responces.")
+   (read-prefix "with a wizard and a named NPC. Give the wizard 3 questions and the NPC 3 responses.")
 
    'npc-4
    (read-prefix "with a wizard and 2 NPCs. Give the wizard and NPCs random dialog and responses.")
    
    'npc-5
-   (read-prefix "with a wizard and an NPC who has a quest to fetch their lost wand. Give the NPC new dialog after the quest is complete.")
+   (read-prefix "an NPC who has a quest to fetch their lost wand. Give the NPC new dialog after the quest is complete.")
 
    ;Spell stimuli
    'weapon-1
    (read-prefix "with a powerful, rare wand. Customize the name and the icon.")
 
    'weapon-2
-   (read-prefix "with a powerful, rare wand; a red Fire Dart spell; and several deatheaters. The spell fires multiple darts at once.")
+   (read-prefix "with several deatheaters; a powerful, rare wand; and a red Fire Dart spell. The spell fires multiple darts at once.")
 
    'weapon-3
-   (read-prefix "with a patronus spell, a wizard, and several deatheaters. The patronus spell fires an animal sprite with high damage but slow speed and short range.")
+   (read-prefix "with a patronus spell and several deatheaters. The patronus spell creates an animal sprite with high damage but slow speed and short range.")
 
    'weapon-4
-   (read-prefix "with 2 different spells, a wizard, and many deatheaters. Add pickup cutscenes to both spells.")
+   (read-prefix "with 2 different spells and many deatheaters. Add pickup cutscenes to both spells.")
 
    'weapon-5
    (read-prefix "with a deatheater and a powerful wand that has pickup and drop cutscenes. Give the deatheater a creative death cutscene.")
--- ts-adventure-mario/katas/read-code-stimuli.rkt
+++ ts-adventure-mario/katas/read-code-stimuli.rkt
@@ -24,7 +24,7 @@
    (read-prefix "with a fast Princess Peach and an intro cutscene. Customize the cutscene with a sprite and some text.")
 
    'avatar-4
-   (read-prefix "with Toad and an intro cutscene. Customize the cutscene with two pages: the 1st with text and the 2nd with a sprite and text.")
+   (read-prefix "with Toad and an intro cutscene. Customize the cutscene with two pages, include sprites and text.")
 
    ;Coin Stimuli
    'coin-1
@@ -40,7 +40,7 @@
    (read-prefix "with a extremely rare and valuable gold coin. When the coin is picked up, spawn a cutscene with a gold coin and some text.")
 
    'coin-5
-   (read-prefix "with 15 coins that do not respawn and an NPC with a collect quest where you have to collect 100 points for a reward.")
+   (read-prefix "with 15 coins that do not respawn and an NPC. Give a collect quest where you have to collect 100 points for a reward.")
 
    ;Level Design Stimuli
    'level-design-1
@@ -66,17 +66,17 @@
    (read-prefix "with an NPC who has a quest to find their lost Cheep. Give the quest an ending cutscene including the Cheep and some text.")
 
    'fetch-quest-3
-   (read-prefix "with an NPC who has two quests. The quests are to find and return 2 different mushrooms.")
+   (read-prefix "with an NPC who has 2 quests. The quests are to find and return 2 different mushrooms; add a reward to 1 of the quests.")
 
    'fetch-quest-4
-   (read-prefix "with an NPC who has a quest to find their lost Goomba. The NPC's dialog changes once the quest is complete.")
+   (read-prefix "with an NPC who has a rewarded quest to find their lost Goomba. The NPC's dialog changes once the quest is complete.")
 
    'fetch-quest-5
    (read-prefix "with a Lakitu NPC who has a high-reward quest to find their lost Spiny. Give the Spiny pickup and drop cutscenes (the drop cutscene only happens if dropped away from Lakitu).")
 
    ;Enemy Stimuli
    'enemy-1
-   (read-prefix "with 10 strong, nocturnal enemies with low intelligence.")
+   (read-prefix "with 10 strong, nocturnal enemies with moderate intelligence.")
 
    'enemy-2
    (read-prefix "with 4 low intelligence Goomba, 2 moderately intelligent Piranha, and a highly intelligent, nocturnal Bowser.")
@@ -85,9 +85,9 @@
    (read-prefix "with a spear and 3 highly intelligent enemies that drop gold coins.")
 
    'enemy-4
-   (read-prefix "with Princess Peach, a spear, and 3 hard enemies that shoot fireballs. Include a creative game over cutscene.")
+   (read-prefix "with Princess Peach, a spear, and 3 highly intelligent enemies that shoot fireballs. Include a creative game over cutscene.")
 
    'enemy-5
-   (read-prefix "with an NPC who has a high-reward quest to slay all enemies. Include 10 random enemies, a fireball powerup, and a game over cutscene.")
+   (read-prefix "with an NPC who has a quest to slay all enemies; the quest has a point and item reward. Include 10 random enemies, a fireball power, and a game over cutscene.")
    
 ))
