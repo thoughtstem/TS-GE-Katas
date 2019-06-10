d34fd680b433420a73a5c2bb66a59f091f4d3cc7
Sonny Najar <sonnynajar@gmail.com>
2019-06-10 10:08:11 -0700

    fixed rendering for hero and sea

--- ts-k2-hero-summer-camp-2019/rendering.rkt
+++ ts-k2-hero-summer-camp-2019/rendering.rkt
@@ -4,7 +4,8 @@
 
 (require scribble/manual
          ts-kata-util/katas/rendering
-         ts-kata-util/katas/main)
+         ts-kata-util/katas/main
+         scribble/core)
 
 ;This is where you define special ways that katas render
 ;  within this collection only -- i.e. if this collection
@@ -13,17 +14,24 @@
 ;If there are no special renderings, don't add anything to this file.
 (define (render #:level (level subsection) kc)
   (kata-collection->scribble
-   #:befores (list (curry kata->title level))
+   #:befores (list kata->title
+                   kata->tip)
    kc))
 
 (require (only-in scribble/manual image para subsection))
 
-(define (kata->title level k)
-  (level (~a (kata-name k) " Kata")))
+(define (kata->title k)
+  (subsection (~a (kata-name k) " Kata")))
+
+(define (kata->tip k)
+  (define tip (kata-tip k))
+  (if tip
+      (if ((listof block?) tip)
+          tip
+          (side-note* "Tip" tip))
+      #f))
 
 (define (kata-section kind collection)
   (list
    (section (kata-id->kata-name kind))
    (render collection)))
-
-
