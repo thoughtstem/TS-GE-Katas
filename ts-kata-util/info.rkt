8362363b8dd5cb219a8bcef909e09fc53124de30
srfoster <stephen@thoughtstem.com>
2019-10-02 12:30:21 -0700

    fixing name of image-coloring package

--- ts-kata-util/info.rkt
+++ ts-kata-util/info.rkt
@@ -6,7 +6,7 @@
                "lang-file"
                "colors"
                "define-assets-from"
-               "image-colors"))
+               "image-coloring"))
 
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/manual.scrbl" ())))
